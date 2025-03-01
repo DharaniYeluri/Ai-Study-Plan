cfc82af50dc3f5c63d139252c538eeb8b2870109
DharaniYeluri <150106465+DharaniYeluri@users.noreply.github.com>
2025-03-01 11:01:56 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<h1>Welcome to Studbud</h1>} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
